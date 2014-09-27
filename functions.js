5a0f025eab66943879aeb7430630ebedf2746d6b
MetaSean <metasean@users.noreply.github.com>
2014-09-27 13:54:17 -0600

    functions done

--- functions.js
+++ functions.js
@@ -1,28 +1,35 @@
+console.log("WELCOME");
+
 //Write a function called 'welcome' that takes in a name (say Tyler), and then returns 'Welcome, Tyler' (or whatever the name was).
 
   //Function definition here
-
+var welcome = function(name) {
+    return ("Welcome, " + name);
+};
 
   //Call the function here
-
+console.log(welcome('Tyler'));
 
 /* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
 
 
-
+console.log("MULTIPLY");
 //Write a function called 'multiply' that takes in two numbers, multiplies them together, and returns the result
 
   //Function definition here
 
+var multiply = function (num1, num2) {
+    return num1 * num2;
+};
+
 
   //Call the function here
-
-
+console.log(multiply(5, 5));
 
 /* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
 
 
-
+console.log("DICE");
 /*Write a function called 'dice' that takes in an array then returns an object
   which has two keys, 'evens' and 'odds'.'evens' is an array full of the even numbers in 
   the original array an 'odds' is an array full of the odd values in the original array
@@ -33,12 +40,42 @@
     obj.evens ----> [2,4,6];
     obj.odds ----> [1,3,5]
 */
+var dice = function(arr) {
+    var obj = {
+        evens: [],
+        odds: []
+    };
+    
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] % 2 == 0) {
+            obj.evens.push(arr[i]);
+        } else {
+            obj.odds.push(arr[i]);
+        }
+    }
+    return obj;
+};
   
+var myArray = [1,2,3,4,5,6];
+var newArray = dice(myArray);
+console.log("evens : " + newArray.evens);
+console.log("odds : " + newArray.odds);
   
 
 /* we have an i5 processor and are currious as to what cores are needed when we are playing Minecraft.
 
 write a function that can take in an array of objects and output the ammount of total processor output by all cores that are in use */
+
+console.log("ALL CORES PROCESSOR OUTPUT");
+var allCores = function(arr) {
+    var allCores = 0;
+    for (var i = 0; i < arr.length; i++) {
+        allCores += arr[i].output;
+    }
+    return allCores;
+}
+        
+
 
 var processors = [{
     core: 1,
@@ -61,3 +98,5 @@ var processors = [{
     inUse: false,
     output: 670
 }];
+
+console.log(allCores(processors));
--- objects.js
+++ objects.js
@@ -1,26 +1,48 @@
-//Create a function called User which has the following properties
+//Create a object called User which has the following properties
 //username, email, password, birthday
 
-  //code here
+var user = {
+  		username: 'Tyler',
+        email: 'tyler@southpark.com',
+        password: 'kenny',
+        birthday: '17/4/90'
+};
 
 
 //edit the username to be 'js_slinger'
   
   //code here
 
+user.username = 'js_slinger';
+console.log(user.username);
+
+
 
 //Add a property of favoriteBook and set it to 'Mike Tyson learns to Whistle'
 
   // code here
-
+user.favoriteBook = 'Mike Tyson learns to Whistle';
+console.log(user.favoriteBook);
 
 //Remove the password property from your object
   
-  // code here
-
+delete user.password;
+console.log(user.password);
 
 //Create an array called 'values'. Loop through your object and push every value
 //from  your object into your 'values' array. So for example, you'll eventually have
 //an array that looks similar to ['js_slinger', 'tm@gmail.com', '5/2/1990'];
 
   // code here 
+
+var values = [];
+for (key in user) {
+    values.push(user[key]);
+}
+console.log (values);
+    
+    
+    
+    
+    
+    
