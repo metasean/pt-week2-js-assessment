d3a326b7a22e5a713871c8cf65c20036ef57bd26
MetaSean <metasean@users.noreply.github.com>
2014-09-27 12:28:43 -0600

    arrays done

--- arrays.js
+++ arrays.js
@@ -1,29 +1,39 @@
 //create an array called family which has your family members in order of their age
-
-  //code here
-
-
+var family = [];
+family[0] = "Ernest";
+family[1] = "Cathrine";
+family[2] = "Sean";
+family[3] = "Ernest";
+console.log(family);
 
 //Remove the oldest member of your family
-
-  //code here
-
-
+family.shift();
+console.log(family);
 
 //Add a new family member as the youngest member
-
-  //code here
-
+family.push("Sandy");
+console.log(family);
 
 //LOOP over your array to see if you have a family member named 'Tyler'
 // If you do, alert ('I have a Tyler'), if you do not, alert ('I have no Tylers')
+tylerFlag = false;
 
-  //Code here
+for (var i = 0; i < family.length; i++) {
+    if (family[i] == 'Tyler') {
+        alert ('I have a Tyler');
+        tylerFlag = true;
+        break;
+    } 
+}
+if (!tylerFlag) {
+    alert ('I have no Tylers');
+}
 
 
 // Remove the second oldest member of your family
 
-  //Code here
+family.splice(1, 1)
+console.log(family);
 
 
 
@@ -31,4 +41,11 @@
 
   //Function definition here
 
+    var reverse = function(string) {
+        var arr = string.split('');
+        arr.reverse();
+        return arr.join('');
+    };
+
   //Call the function here
+    console.log(reverse("Sean"));
