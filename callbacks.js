eb5d4a783e777a6685aeb2e8abe51d8153796820
MetaSean <metasean@users.noreply.github.com>
2014-09-27 14:30:53 -0600

    callbacks done

--- callbacks.js
+++ callbacks.js
@@ -1,11 +1,35 @@
-//Make the following function calls work
-  giveItName('name', function(){
-    alert('Your name is ' + name);
-  });
 
-  var nums = [2,4,5,6];
-  //returns true if all items in the array are evens.
-  areEvens(nums, function(item){
+var giveItName = function (name, callback) {
+	callback(name);
+}
+
+var alertName = function (name) {
+  alert('Your name is ' + name);  
+}
+
+//Make the following function calls work
+  giveItName('Sean', alertName);
+
+
+var nums = [2,4,5,6];
+//returns true if all items in the array are evens.
+
+var areEvens = function(numArr, callback) {
+    var allEvens = true;
+    for (var i = 0; i < numArr.length; i++) {
+        if (!callback(numArr[i])) {
+            allEvens = false;
+        }
+    }
+    return allEvens;
+};
+
+var isEven = function(item){
     return item % 2 === 0; 
-  });
+};
+
+console.log( areEvens(nums, isEven));
+
+
+
 
